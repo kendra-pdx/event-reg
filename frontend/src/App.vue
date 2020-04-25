de023ba5d18b710d78cfe767924cf6245b80a05e
kender elford <kender@enkode.me>
2020-04-25 15:33:56 -0700

    app as component

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <create-profile v-on:created="updateCreatedProfile"></create-profile>
+    <hr />
+    <user-login ref="userLogin"></user-login>
+  </div>
+</template>
+
+<script>
+import CreateProfile from "./CreateProfile.vue";
+import UserLogin from "./UserLogin.vue";
+
+export default {
+  components: {
+    "create-profile": CreateProfile,
+    "user-login": UserLogin
+  },
+  methods: {
+    updateCreatedProfile: function(profile) {
+      console.log("new profile", profile);
+      this.$refs.userLogin.email = profile.email;
+    }
+  }
+};
+</script>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,9 @@
 import _ from "lodash";
 import Vue from "vue";
 
-import CreateProfile from "./CreateProfile.vue"
-import UserLogin from "./UserLogin.vue"
-
+import App from "./App.vue"
 var app = new Vue({
-  el: "#app",
-
-  components: {
-    "create-profile": CreateProfile,
-    "user-login": UserLogin
-  },
-
-  data: {
-  },
-
-  methods: {
-    updateCreatedProfile: (profile) => {
-      console.log("new profile", profile)
-      app.$refs.userLogin.email = profile.email
-    },
-  }
+  el: '#app',
+  template: '<App/>',
+  components: { App }
 })
